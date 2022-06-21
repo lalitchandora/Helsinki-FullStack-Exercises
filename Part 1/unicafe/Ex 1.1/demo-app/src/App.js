caf3c19a33efd87b0c5415897746d9f9e43e0a01
Lalit chandora <lalit.n.chandora@gmail.com>
2022-06-21 17:12:53 +0530

    Ex 1.1 - 1.5

--- Part 1/unicafe/Ex 1.1/demo-app/src/App.js
+++ Part 1/unicafe/Ex 1.1/demo-app/src/App.js
@@ -3,19 +3,29 @@ import Content from "./components/Content";
 import Total from "./components/Total";
 
 const App = () => {
-	const course = 'Half Stack application development'
-	const part1 = 'Fundamentals of React'
-	const exercises1 = 10
-	const part2 = 'Using props to pass data'
-	const exercises2 = 7
-	const part3 = 'State of a component'
-	const exercises3 = 14
+	const course = {
+		name: 'Half Stack application development',
+		part: [
+			{
+				name: 'Fundamentals of React',
+				exercise: 10
+			},
+			{
+				name: 'Using props to pass data',
+				exercise: 7
+			},
+			{
+				name: 'State of a component',
+				exercise: 14
+			}
+		]
+	}
 
 	return (
 		<div>
-			<Header course={course}/>
-			<Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-			<Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+			<Header course={course.name}/>
+			<Content parts={course.part}/>
+			<Total parts={course.part}/>
 		</div>
 	);
 }
